0bf5d1cf349bcac2e316ea599d92a9820f3cd89a
Mahir Aman <eddieaman475@gmail.com>
2026-07-22 13:32:08 +0300

    Make the public home page static

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-import { Dashboard } from "../components/dashboard";
 import { MarketingHome } from "../components/marketing-home";
-import { getCurrentUserContext } from "../lib/data/context";
-import { getDashboardSnapshot } from "../lib/data/erp";
 
-export const dynamic = "force-dynamic";
+export const revalidate = 3600;
 
-export default async function HomePage() {
-  const user = await getCurrentUserContext();
-  if (!user) return <MarketingHome />;
-  const snapshot = await getDashboardSnapshot();
-  return <Dashboard snapshot={snapshot} user={user} />;
+export default function HomePage() {
+  return <MarketingHome />;
 }
