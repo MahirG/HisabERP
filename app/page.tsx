cab43b851302c05514ea9cfd7f6272cc217c092d
Mahir Aman <eddieaman475@gmail.com>
2026-07-25 04:50:20 +0300

    Load homepage banner carousel styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { MarketingHome } from "../components/marketing-home";
 import "./apple-smooth-public.css";
 import "./home-office-hero.css";
+import "./home-banner-slider.css";
 
 export const dynamic = "force-static";
 export const revalidate = 3600;
