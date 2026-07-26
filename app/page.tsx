d34cdda6b523fbbe84a90665e99d9c715511f3dc
Mahir Aman <eddieaman475@gmail.com>
2026-07-26 13:28:08 +0300

    Remove homepage banner image styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { MarketingHome } from "../components/marketing-home";
 import "./apple-smooth-public.css";
-import "./home-office-hero.css";
-import "./home-banner-slider.css";
+import "./home-text-hero.css";
 
 export const dynamic = "force-static";
 export const revalidate = 3600;
